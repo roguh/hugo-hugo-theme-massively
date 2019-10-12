37e49be568c6ca19f0930b6abafaf5579b58c4cb
Hugo Rivera <6373447+roguh@users.noreply.github.com>
2019-10-12 05:22:15 -0700

    JPG compression

--- static/images/convert.sh
+++ static/images/convert.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set +x
 inkscape --export-png=background.png background.svg
-convert background.png -quality 95 background.jpg
+convert background.png -strip -interlace Plane -gaussian-blur 0.05 -quality 95 background.jpg
 # convert background.png -quality 50 -define webp:lossless=true background.webp
 convert background.png -quality 50 background.webp
